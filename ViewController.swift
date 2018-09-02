e3a319db4a3c72bd320b0bcc387f1ff2c2e3a4e1
Rayees <mir.rayees@aquera.com>
2018-09-02 12:05:27 +0530

    Version 2

--- ViewController.swift
+++ ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  ff
-//
-//  Created by thincnext on 10/07/18.
-//  Copyright © 2018 thincnext. All rights reserved.
-//
 
 import UIKit
 
@@ -12,12 +5,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    func log(){
+        print("Log")
+        
     }
 
 
