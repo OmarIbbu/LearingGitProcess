745b55ec05cb78e9db90d2d3159e880278f93a99
thincnext <thincnext@deadlyStation.local>
2018-09-02 12:01:48 +0530

    Version 1

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ff
+//
+//  Created by thincnext on 10/07/18.
+//  Copyright © 2018 thincnext. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
